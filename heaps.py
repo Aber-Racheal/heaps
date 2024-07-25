1fd2cac036a449a8069cf520960c7fe09cc90b02
Racheal Aber <158446264+Aber-Racheal@users.noreply.github.com>
2024-07-25 17:59:30 +0300

    Update heaps.py

--- heaps.py
+++ heaps.py
@@ -13,55 +13,44 @@ class MinHeap:
         self.Heap[0] = -1 * sys.maxsize 
         self.FRONT = 1
   
-    # Function to return the position of 
-    # parent for the node currently 
-    # at pos 
+
     def parent(self, pos): 
         return pos//2
   
-    # Function to return the position of 
-    # the left child for the node currently 
-    # at pos 
+ 
     def leftChild(self, pos): 
         return 2 * pos 
   
-    # Function to return the position of 
-    # the right child for the node currently 
-    # at pos 
+
     def rightChild(self, pos): 
         return (2 * pos) + 1
   
-    # Function that returns true if the passed 
-    # node is a leaf node 
+
     def isLeaf(self, pos): 
         return pos*2 > self.size 
   
-    # Function to swap two nodes of the heap 
+
     def swap(self, fpos, spos): 
         self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos] 
   
-    # Function to heapify the node at pos 
+
     def minHeapify(self, pos): 
   
-        # If the node is a non-leaf node and greater 
-        # than any of its child 
         if not self.isLeaf(pos): 
             if (self.Heap[pos] > self.Heap[self.leftChild(pos)] or 
                self.Heap[pos] > self.Heap[self.rightChild(pos)]): 
   
-                # Swap with the left child and heapify 
-                # the left child 
+
                 if self.Heap[self.leftChild(pos)] < self.Heap[self.rightChild(pos)]: 
                     self.swap(pos, self.leftChild(pos)) 
                     self.minHeapify(self.leftChild(pos)) 
   
-                # Swap with the right child and heapify 
-                # the right child 
+      
                 else: 
                     self.swap(pos, self.rightChild(pos)) 
                     self.minHeapify(self.rightChild(pos)) 
   
-    # Function to insert a node into the heap 
+    
     def insert(self, element): 
         if self.size >= self.maxsize : 
             return
@@ -74,22 +63,18 @@ class MinHeap:
             self.swap(current, self.parent(current)) 
             current = self.parent(current) 
   
-    # Function to print the contents of the heap 
+  
     def Print(self): 
         for i in range(1, (self.size//2)+1): 
             print(" PARENT : "+ str(self.Heap[i])+" LEFT CHILD : "+ 
                                 str(self.Heap[2 * i])+" RIGHT CHILD : "+
-                                str(self.Heap[2 * i + 1])) 
-  
-    # Function to build the min heap using 
-    # the minHeapify function 
+                                str(self.Heap[2 * i + 1]))
     def minHeap(self): 
   
         for pos in range(self.size//2, 0, -1): 
             self.minHeapify(pos) 
   
-    # Function to remove and return the minimum 
-    # element from the heap 
+
     def remove(self): 
   
         popped = self.Heap[self.FRONT] 
@@ -97,8 +82,7 @@ class MinHeap:
         self.size-= 1
         self.minHeapify(self.FRONT) 
         return popped 
-  
-# Driver Code 
+
 if __name__ == "__main__": 
       
     print('The minHeap is ') 
